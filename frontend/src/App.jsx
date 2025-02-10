451977ed710cc362c88a42c1cd86df2a9f2a25c6
Yisongjing <24aw0105@jec.ac.jp>
2025-02-10 16:30:08 +0900

    1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import PokemonDetails from "./PokemonDetails";
 import "./App.css";
 
-const API_URL = "https://pokemon-back-pearl.vercel.app/";
+const API_URL = "https://pokemon-back-pearl.vercel.app";
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
